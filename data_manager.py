95684c8aa47decce2b6bc972a7220b2b0e1ae5f2
Amrita Jawaley <88776344+Jawaley@users.noreply.github.com>
2023-08-08 15:41:16 +0530

    Create data_manager.py

---
+++ data_manager.py
@@ -0,0 +1,58 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+ST_API_PRICES = os.getenv("ST_API_PRICES")
+ST_API_USERS = os.getenv("ST_API_USERS")
+ST_HEADER = {
+    "Authorization": f"Bearer {os.getenv('ST_TOKEN')}",
+    "Content-Type": "application/json",
+}
+
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.all_cities = self.__get_cities()
+       
+    def __get_cities(self):
+        """Sets a list of city dictionaries with name, (IATA), lowest price and ID.
+
+        """
+        with requests.Session() as session:
+            response = session.get(url=ST_API_PRICES, headers=ST_HEADER)
+            response.raise_for_status()
+            return response.json()["prices"]
+    
+    def insert_iata(self, city_data: dict, city_iata: str):
+        """Inserts IATA code for the corresponding city.
+
+        Args:
+            city_data (dict): A city dictionary with name, lowest price and ID.
+            city_iata (str): IATA code for the city.
+        """
+        iata_code = {
+            "price": {
+                "iataCode": city_iata
+            }
+        }
+        
+        with requests.Session() as session:
+            response = session.put(url=f"{ST_API_PRICES}/{city_data['id']}", json=iata_code, headers=ST_HEADER)
+            response.raise_for_status()
+        
+    def get_subscribers(self) -> list:
+        """Fetches all the user information from the spreadsheet and returns the user list.
+
+        Returns:
+            list: List of the user dictionaries with first name, last name and email address.
+        """
+        with requests.Session() as session:
+            response = session.get(url=f"{ST_API_USERS}", headers=ST_HEADER)
+            response.raise_for_status()
+            return response.json()["users"]
+            
